47c88c79007d90b737eccd522990ff2086587fff
ManWithDreams <103361329+WalterWhite123@users.noreply.github.com>
2026-01-11 19:32:14 +0100

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
+    
+    //Computer choice 
+    
     //Give a random number between 0 and 2
-    
-    
     function randomNumber(){
         let randomNumber = Math.random()*2;
         let randomNumberRounded = Math.round(randomNumber);
         return randomNumberRounded;
     }
     
-    
+    // The computer choice
     function getComputerChoice(){
         let computerChoice;
         switch (randomNumber()){
             case 0: 
-                computerChoice = "Rock";
+                computerChoice = "rock";
                 break;
             case 1:
-                computerChoice = "Paper";
+                computerChoice = "paper";
                 break;
             case 2:
-                computerChoice = "Scissors";
+                computerChoice = "scissors";
                 break;
     } 
     return computerChoice;
     }
 
+    //Test
     console.log(getComputerChoice());
+
+    
+
+    //Human choice 
+    //Prendre l'input de son choix | ok
+    //Stocker dans une variable son choix | ok
+    // S'assurer que le choix est bon (les choix dispo, bonne écriture, tout en minuscule) \ pas ok
+    
+
+    function getHumanChoice(){
+        const player = prompt("Rock, paper or scissors");
+        return player;
+    }
+    //test
+    console.log(getHumanChoice());
+
+   
