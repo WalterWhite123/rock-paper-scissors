3d9123743cbdcd7ae3c762c672a348159c6f774e
ManWithDreams <103361329+WalterWhite123@users.noreply.github.com>
2026-01-11 19:20:44 +0100

    Robot choice done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("hello");
+    //Give a random number between 0 and 2
+    
+    
+    function randomNumber(){
+        let randomNumber = Math.random()*2;
+        let randomNumberRounded = Math.round(randomNumber);
+        return randomNumberRounded;
+    }
+    
+    
+    function getComputerChoice(){
+        let computerChoice;
+        switch (randomNumber()){
+            case 0: 
+                computerChoice = "Rock";
+                break;
+            case 1:
+                computerChoice = "Paper";
+                break;
+            case 2:
+                computerChoice = "Scissors";
+                break;
+    } 
+    return computerChoice;
+    }
+
+    console.log(getComputerChoice());
